bae667c63d96272a9989ee5e67376575992a962d
Daniel A. Queiroz <daki1239@gmail.com>
2023-05-23 23:48:49 -0300

    first commit

---
+++ api/controllers/user.js
@@ -0,0 +1,55 @@
+import { db } from "../db.js";
+
+export const getUsers = (_, res) => {
+  const q = "SELECT * FROM tabusuario;";
+
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data);
+  });
+};
+
+export const addUser = (req, res) => {
+  
+  const q =
+    "INSERT INTO tabusuario (TabUsuarioNome, TabUsuarioCpf, TabUsuarioMesa) VALUES (?)";
+  
+  const values = [
+    req.body.nome,
+    req.body.cpf,
+    req.body.mesa
+  ];
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário criado com sucesso.");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE TabUsuario SET TabUsuarioNome = ?, TabUsuarioCpf = ?, TabUsuarioMesa = ? WHERE idTabUsuario = ?";
+
+  const values = [
+    req.body.nome,
+    req.body.cpf,
+    req.body.mesa,
+  ];
+
+  db.query(q, [...values, req.params.idTabUsuario], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário atualizado com sucesso.");
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM TabUsuario WHERE idTabUsuario = ?";
+
+  db.query(q, [req.params.idTabUsuario], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário deletado com sucesso.");
+  });
+};
---
+++ frontend/src/components/Form.js
@@ -0,0 +1,115 @@
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Label = styled.label``;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+`;
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.nome.value = onEdit.TabUsuarioNome;
+      user.cpf.value = onEdit.TabUsuarioCpf;
+      user.mesa.value = onEdit.TabUsuarioMesa;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = ref.current;
+    if (
+      !user.nome.value ||
+      !user.cpf.value ||
+      !user.mesa.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+    
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/edit/" + onEdit.idTabUsuario, {
+          nome: user.nome.value,
+          cpf: user.cpf.value,
+          mesa: user.mesa.value
+        }) 
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          nome: user.nome.value,
+          cpf: user.cpf.value,
+          mesa: user.mesa.value
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.cpf.value = "";
+    user.mesa.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>CPF</Label>
+        <Input name="cpf" />
+      </InputArea>
+      <InputArea>
+        <Label>Mesa</Label>
+        <Input name="mesa" />
+      </InputArea>
+
+      <Button type="submit">Abrir Cardápio</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
